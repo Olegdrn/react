7d85079814cdafdceb1e3e59615e1f8227ab0baf
Olegdrn <105755089+Olegdrn@users.noreply.github.com>
2022-10-20 19:10:21 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Message } from './components/Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/Message.jsx
@@ -0,0 +1,7 @@
+import './Message.css';
+
+export function Message() {
+  return <>
+    <p>кукусики</p></>
+}
+
