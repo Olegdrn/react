e4ded26c90de28255dced26ca95dd03ecf621cfb
Olegdrn <105755089+Olegdrn@users.noreply.github.com>
2022-10-24 18:01:29 +0300

    Add files via upload

---
+++ src/components/messageList.jsx
@@ -0,0 +1,54 @@
+
+export function MessageList(props) {
+    return <>
+        <p>{props.messages}</p></>
+}
+
+// import React, { useState } from 'react';
+
+// export function MessageList() {
+//     const [messages, setMessages] = useState([
+//         "message 1"
+//     ]);
+
+//     return (
+//         <form onSubmit={submitForm}>
+//             <input placeholder='Имя' value={text} onChange={(e) =>
+//                 setData(pervstate => ({ ...pervstate, text: e.target.value })
+//                 )} />
+//             <button type='submit'>Отправить</button>
+//         </form>
+//     )
+    // return messages.map((message) => <div className='paragph3'>{message}</div>);
+
+// }
+
+// export function MessageList = ({ data, setData, setMessage }) => {
+//     const { text, author } = data;
+//     const submitForm = (event) => {
+//         event.preventDefault()
+//         if (text.length > 0) {
+//             setMessage(pervstate => [...pervstate, { text, author }])
+//         }
+//         setData(
+//             {
+//                 text: '',
+//                 author: ''
+//             }
+//         )
+//     }
+
+//     return (
+//         <form onSubmit={submitForm}>
+//             <input placeholder='Имя' value={text} onChange={(e) =>
+//                 setData(pervstate => ({ ...pervstate, text: e.target.value })
+//                 )} />
+//             <input placeholder='Текс' value={author} onChange={(e) =>
+//                 setData(pervstate => ({ ...pervstate, author: e.target.value })
+//                 )} />
+
+//             <button type='submit'>Отправить</button>
+//         </form>
+//     )
+// }
+
